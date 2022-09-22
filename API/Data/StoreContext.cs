64700fdc85c56b318b7e7ac49e7f724d3c5d9610
Awet9I <89390508+Awet9I@users.noreply.github.com>
2022-09-22 11:53:44 +0200

    basic api's of entity product

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+       //sets up the table for the entity product
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ API/WeatherForecast.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace API
+{
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string Summary { get; set; }
+
+    //string? in .net 6 means that the string is nullable
+}
+
+}
+
